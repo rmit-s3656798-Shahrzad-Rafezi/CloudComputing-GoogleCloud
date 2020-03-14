afba5b2b30154e070804e6e68d6ead0dae9700ac
Nicholas White <nickobelic1999@gmail.com>
2020-03-14 12:48:01 +1100

    put in example code

--- login.php
+++ login.php
@@ -1,8 +1,41 @@
 <?php
 
+# Includes the autoloader for libraries installed with composer
+require __DIR__ . '/vendor/autoload.php';
+
 use Google\Cloud\Datastore\DatastoreClient;
 
+# Your Google Cloud Platform project ID
+$projectId = 's3656798-cc2020';
+
 session_start();
+
+/*
+
+# Instantiates a client
+$datastore = new DatastoreClient([
+    'projectId' => $projectId
+]);
+
+# The kind for the new entity
+$kind = 'Task';
+
+# The name/ID for the new entity
+$name = 'sampletask1';
+
+# The Cloud Datastore key for the new entity
+$taskKey = $datastore->key($kind, $name);
+
+# Prepares the new entity
+$task = $datastore->entity($taskKey, ['description' => 'Buy milk']);
+
+# Saves the entity
+$datastore->upsert($task);
+
+echo 'Saved ' . $task->key() . ': ' . $task['description'] . PHP_EOL;
+*/
+
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     //sets up database
