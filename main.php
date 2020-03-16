89baa5d24a109ac67fda08dbcf12eae2f9cc335c
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-03-16 18:11:56 +1100

    Added redirect buttons into main.php
    Someone asked if we can use JS to redirect and they said yes. so if you know a proper way to redirect using php, then you are welcome to change it

--- main.php
+++ main.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
+
     <head>
         <title>Main Page</title>
     </head>
 
     <body>
         <h3>Success!</h3>
+        <input type="button" onclick="window.location='/name'" value="Change Name"/>
+        <input type="button" onclick="window.location='/password'" value="Change Password"/>
     </body>
+
 </html>
---
+++ name.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Name Page</title>
+    </head>
+
+    <body>
+        <h3>Success Name Page!</h3>
+    </body>
+
+</html>
