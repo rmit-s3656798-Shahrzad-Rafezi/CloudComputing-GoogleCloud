824dfc19a8db8b01da7415349952412869f9438c
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-03-18 14:24:44 +1100

    added another label to enter old password

--- password.php
+++ password.php
@@ -1,6 +1,6 @@
 <?php
 
-$password = $_POST['password'];
+$password = $_POST['old_password'];
 
 if( isset($password ) && empty($password ) ){
     echo "<h3>password cannot be empty</h3>";
@@ -8,7 +8,7 @@ if( isset($password ) && empty($password ) ){
 
 //will have to a query to look into database to see if the old password exists. if yes, then update the password.
 elseif ( isset($password ) ) {
-    $entity['password'] = $_POST['password'];
+    $entity['password'] = $_POST['new_password'];
     $data_store->update($entity);
     echo "<script type='text/javascript'> window.location='/login'; </script>";
 }
@@ -26,8 +26,10 @@ elseif ( isset($password ) ) {
         <h3>Success Password Page!</h3>
 
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <label>Password</label>
-            <input type="string" name="password" placeholder="Enter password" />
+            <label>Old Password</label>
+            <input type="string" name="old_password" placeholder="Enter Old password" />
+            <label>New Password</label>
+            <input type="string" name="new_password" placeholder="Enter Old password" />
             <input type="submit" value="submit" />
         </form>
 
