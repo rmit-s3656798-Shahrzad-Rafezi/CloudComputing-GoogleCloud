b1df423c7da947bc1c875b32613ba18cb164d990
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-03-18 14:15:27 +1100

    added update functionality for name and password
    I did not test it but it would be good to write it down before I forget.

--- name.php
+++ name.php
@@ -1,3 +1,18 @@
+<?php
+
+$name = $_POST['name'];
+
+if( isset($name) && empty($name) ){
+    echo "<h3>name cannot be empty</h3>";
+}
+
+elseif ( isset($name) ) {
+    $entity['name'] = $name;
+    $data_store->update($entity);
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -6,7 +21,15 @@
     </head>
 
     <body>
+
         <h3>Success Name Page!</h3>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <label>Name</label>
+            <input type="string" name="name" placeholder="Enter name" />
+            <input type="submit" value="submit" />
+        </form>
+
     </body>
 
 </html>
--- password.php
+++ password.php
@@ -1,3 +1,18 @@
+<?php
+
+$password = $_POST['password'];
+
+if( isset($password ) && empty($password ) ){
+    echo "<h3>password cannot be empty</h3>";
+}
+
+elseif ( isset($password ) ) {
+    $entity['password'] = $_POST['password'];
+    $data_store->update($entity);
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -7,6 +22,13 @@
 
     <body>
         <h3>Success Password Page!</h3>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <label>Password</label>
+            <input type="string" name="password" placeholder="Enter password" />
+            <input type="submit" value="submit" />
+        </form>
+
     </body>
 
 </html>
