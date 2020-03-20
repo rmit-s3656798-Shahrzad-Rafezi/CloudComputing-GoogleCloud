b4d7c056643db672e8ed528f732b0a21125386ee
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-03-21 09:39:59 +1100

    password.php working!!!!

--- password.php
+++ password.php
@@ -1,20 +1,41 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+use Google\Cloud\Datastore\DatastoreClient;
+
+session_start();
+
+//My Google Cloud Platform project ID
+$projectId = 's3656798-cc2020';
+
+//Sets up database
+$data_store = new DatastoreClient([
+    'keyFilePath' => 's3656798-cc2020-6770f1694940.json',
+    'projectId' => $projectId
+]);
+
 $password = $_POST['old_password'];
 
-if( isset($password ) && empty($password ) ){
+if( isset( $password ) && empty( $password ) ){
     echo "<h3>password cannot be empty</h3>";
 }
 
-//will have to do a query to look into database to see if the old password exists. if yes, then update the password.
-elseif ( isset($password ) ) {
-    foreach ($result as $properties => $users) {
+elseif ( isset( $password ) ) {
+
+    $query = $data_store->gqlQuery('SELECT * FROM User WHERE id = @id', ['bindings'=>['id'=>$_SESSION['authenticated']]]);
+    $result = $data_store->runQuery($query);
+
+    foreach ($result as $users) {
+
         if ( $password == $users['password'] ){
-            $entity['password'] = $_POST['new_password'];
-            $data_store->update($entity);
-            echo "<script type='text/javascript'> window.location='/login'; </script>";
+            $users->password = (int)$_POST['new_password'];
+            $data_store->update($users);
+            header('Location: /.*');
+            die();
         }
+
     }
+
     if( $password != $users['password'] ) {
         echo "<h3>Incorrect Password</h3>";
     }
@@ -25,24 +46,24 @@ elseif ( isset($password ) ) {
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <title>Password Page</title>
-    </head>
+<head>
+    <title>Password Page</title>
+</head>
 
-    <body>
+<body>
 
-        <h3>Change Your Password Here</h3>
+<h3>Change Your Password Here</h3>
 
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <label>Old Password</label>
-            <input type="string" name="old_password" placeholder="Enter Old password" />
-            <br />
-            <label>New Password</label>
-            <input type="string" name="new_password" placeholder="Enter Old password" />
-            <br />
-            <input type="submit" value="submit" />
-        </form>
+<form action="#" method="post">
+    <label>Old Password</label>
+    <input type="password" name="old_password" placeholder="Enter Old password" />
+    <br />
+    <label>New Password</label>
+    <input type="password" name="new_password" placeholder="Enter New password" />
+    <br />
+    <input type="submit" value="submit" />
+</form>
 
-    </body>
+</body>
 
 </html>
